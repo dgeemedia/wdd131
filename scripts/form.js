619157c7b7d6928fe653806e6e46ff583002f808
dgeemedia <lolumah@byupathway.edu>
2025-04-13 12:28:58 +0100

    done

--- scripts/form.js
+++ scripts/form.js
@@ -1,3 +1,6 @@
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
+
 const products = [
     { id: "fc-1888", name: "John the Electrician", averagerating: 4.5 },
     { id: "fc-2050", name: "Lagos Express Runner", averagerating: 4.7 },
@@ -6,15 +9,31 @@ const products = [
     { id: "jj-1969", name: "Fast Boda Delivery", averagerating: 5.0 }
   ];
   
-  const select = document.getElementById("product");
-  
-  products.forEach(product => {
+const select = document.getElementById("product");
+products.forEach(product => {
     const option = document.createElement("option");
     option.value = product.id;
     option.textContent = product.name;
     select.appendChild(option);
   });
 
-  document.getElementById("currentyear").textContent = new Date().getFullYear();
-  document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
+  // Check if review count exists, otherwise start at 0
+  let reviewCount = localStorage.getItem('reviewCount') || 0;
+
+  // Convert to number and increment
+  reviewCount = Number(reviewCount) + 1;
+
+  // Store it back
+  localStorage.setItem('reviewCount', reviewCount);
+
+  // Optional: Display it
+  const message = document.createElement('p');
+  message.textContent = `You have submitted ${reviewCount} review(s).`;
+  msg.style.textAlign = "center";
+  msg.style.marginTop = "20px";
+  msg.style.color = "#022142";
+  document.body.appendChild(message);
+
+
+  
   
